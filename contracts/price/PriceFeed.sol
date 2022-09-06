475d403f28c095b521d28e4b57c9d7156d3a0a30
Paddy Jiang <paddy.jiang@maxity.io>
2022-09-06 19:19:49 +0800

    feat: init

---
+++ contracts/price/PriceFeed.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceFeed is Ownable {
+
+    AggregatorV3Interface internal priceFeed;
+
+    constructor(address feed) {
+        setPriceAddress(feed);
+    }
+
+    function setPriceAddress(address feed) public onlyOwner {
+        priceFeed = AggregatorV3Interface(feed);
+    }
+
+    function decimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+
+    function getRoundData(uint80 id) public view returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound) {
+        return priceFeed.getRoundData(id);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+        /*uint80 roundID*/,
+        int price,
+        /*uint startedAt*/,
+        /*uint timeStamp*/,
+        /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      mining: {
+        auto: true,
+        interval: 1000
+      }
+    },
+    localhost: {
+      url: `http://localhost:8545`,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    },
+  },
+};
+
+export default config;
